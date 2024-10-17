1fcdd1bc06b4e6d7ab35a1717cbf3d65d1a9929b
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2024-10-17 11:26:04 -0300

    /revisaotri2odsc

---
+++ Revisao2Tri2oDSC/VARIAVEIS/script.js
@@ -0,0 +1,9 @@
+const comidas =  document.querySelector('.comidas');
+
+const comida1 = prompt('Digite sua comida nº1')
+
+const comida2 = prompt('Digite sua comida nº2')
+
+comidas.innerText = comida1
+
+comidas.innerText = comida2
