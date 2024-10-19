2bffbea64c8ea066acaf2cf84b4ec57ec60ee025
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2024-10-18 22:01:30 -0300

    /array01

---
+++ Array01/script.js
@@ -0,0 +1,15 @@
+// VARIAVEIS UNITÁRIAS
+
+const name = 'Jonáss';
+const name2 = 'Chagáss';
+const name3 = 'Broxuri';
+
+// VARIAVEIS DE ARRAY
+
+const nomes = ['Jonáss', 'Chagáss', 'Broxuri'];
+
+nomes[10] = 'RatoGuy'; // CRIANDO DINAMICAMENTE
+
+console.log(nomes)
+console.log(nomes[2])
+console.log(nomes[10])
