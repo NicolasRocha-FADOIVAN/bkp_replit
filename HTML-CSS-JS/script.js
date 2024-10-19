bafc6ce664e362870ee96d80e171d7d86a5e925d
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2024-10-18 22:26:40 -0300

    /htmlcssjs

---
+++ HTML-CSS-JS/script.js
@@ -0,0 +1,11 @@
+const idade = 17
+const nome2 = "Sigma"
+const sobrenome = "da Bahia"
+const telefone = 55 - 55 - 55 - 55
+const estacasado = true
+
+console.log(idade)
+console.log(nome2)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estacasado)
