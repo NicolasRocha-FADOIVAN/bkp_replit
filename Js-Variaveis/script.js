fec1e6d64f357a9796f7894e7072cd77fd3a9671
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2024-10-18 22:05:42 -0300

    /jsvariaveis

---
+++ Js-Variaveis/script.js
@@ -0,0 +1,26 @@
+// Localizar Elementos do HTML
+
+const nome = document.getElementById("nome");
+const sobrenome = document.getElementById("sobrenome");
+const email = document.getElementById("email");
+const telefone = document.getElementById("telefone");
+const idade = document.getElementById("idade");
+const sexo = document.getElementById("sexo");
+
+// Criar Variáveis
+
+const novoNome = "Ratuê"
+const novoSobrenome = "calvatico Rosinha"
+const novoEmail = "rataoguei@gamil.con"
+const novoTelefone = "liga pro sequiuaidiq"
+const novoIdade = "69"
+const novoSexo = "Feminine"
+
+// Atribuir Valores ao HTML com os Valores das Nossas Variáveis
+
+nome.innerText = novoNome;
+sobrenome.innerText = novoSobrenome;
+email.innerText = novoEmail;
+telefone.innerText = novoTelefone;
+idade.innerText = novoIdade;
+sexo.innerText = novoSexo;
